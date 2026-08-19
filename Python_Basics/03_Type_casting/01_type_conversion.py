32b27091f90498a5168b7ba1b675f95dcd3e6502
DikshyaKhanal123 <dikshyakhanal64@gmail.com>
2026-08-19 19:17:19 +0545

    type casting

---
+++ Python_Basics/03_Type_casting/01_type_conversion.py
@@ -0,0 +1,6 @@
+a = 10
+b = 5
+print(type(a/3)) #float
+
+ans = 5 +  10.00 
+print(type(ans)) #float
---
+++ Python_Basics/03_Type_casting/02_type_casting.py
@@ -0,0 +1,9 @@
+a = int (5+20.00)
+print(a)
+print(type(a))
+
+b = int("1234")
+print(type(b))
+
+val = bool(10)
+print( val, type(val))
