a0de12b7a5e390163cdf0e1b4e95318e1fa3e0d0
DikshyaKhanal123 <dikshyakhanal64@gmail.com>
2026-08-20 22:22:24 +0545

    conditional statement

--- Python_Basics/04_conditional_statement/contition.py
+++ Python_Basics/04_conditional_statement/contition.py
@@ -3,10 +3,10 @@ gpa = 3.6
 if gpa >=3.6:
     print("A+")
 
-elif gpa>=3.2 and gpa<3.6 :
+elif gpa >= 3.2 and gpa < 3.6 :
     print("A")
 
-elif gpa<3.2 and gpa >2.8 :
+elif gpa < 3.2 and gpa > 2.8 :
     print("B+")
 
 else:
