6f1293e3e3c94cdded04c75099614c96f9e97cde
DikshyaKhanal123 <dikshyakhanal64@gmail.com>
2026-08-30 19:45:28 +0545

    for loop

--- Python_Basics/05_Loop/04_for_loop.py
+++ Python_Basics/05_Loop/04_for_loop.py
@@ -1,2 +1,9 @@
-for i in range(1, 6):
+for i in range(1, 6):  #for sequence of numbers
     print(i)
+
+string = "dikshya"
+for i in string: #for string
+    print(i)
+
+if "i" in string :
+    print("i exists")
