973438e51155351b629e7efb7563da3218e710ac
DikshyaKhanal123 <dikshyakhanal64@gmail.com>
2026-09-08 23:37:13 +0545

    OOP

--- Python_OOP/02_constructor/01_constructor.py
+++ Python_OOP/02_constructor/01_constructor.py
@@ -1,13 +1,16 @@
 class Student:
-    def __init__(self,name,age):
+    def __init__(self,name,age,gpa):
         self.name = name
         self.age = age
+        self.gpa = gpa
+
     def display(self):
         print("name:", self.name)
         print("age:", self.age)
+        print("gpa:" ,self.gpa)
 
-stu1 = Student("Dikshya", 19)
-stu2 = Student("Ram", 30)
+stu1 = Student("Dikshya", 19.3.2)
+stu2 = Student("Ram", 30.4.0)
 
 stu1.display()
 stu2.display()
