8e579220a2912ad1bc26ae3ea669a9d2ae6a0e12
DikshyaKhanal123 <dikshyakhanal64@gmail.com>
2026-09-14 22:59:54 +0545

    file io

--- Python_Advanced_Basics/01_File_handling/05_word_search.py
+++ Python_Advanced_Basics/01_File_handling/05_word_search.py
@@ -1,1 +1,12 @@
-f = open("student.txt")
+data = True
+line = 1
+
+with open("student.txt", "r") as f:
+    while data:
+        data = f.readline()
+        
+        if("Dikshya" in data):
+            print("word found")
+            break
+        
+        line += 1
