8ae335efcbfabe52a8e226c0eff3f64f503ca3ee
DikshyaKhanal123 <dikshyakhanal64@gmail.com>
2026-09-21 08:38:55 +0545

    temperature converter

---
+++ Projects/07_temperature_converter/code.py
@@ -0,0 +1,51 @@
+def celsius_to_fahrenheit():
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = (celsius * 9 / 5) + 32
+    print(f"Temperature: {fahrenheit}°F")
+
+
+def fahrenheit_to_celsius():
+    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+    celsius = (fahrenheit - 32) * 5 / 9
+    print(f"Tempedef celsius_to_fahrenheit():
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = (celsius * 9 / 5) + 32
+    print(f"Temperature: {fahrenheit}°F")
+
+
+def fahrenheit_to_celsius():
+    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+    celsius = (fahrenheit - 32) * 5 / 9
+    print(f"Temperature: {celsius}°C")
+
+
+print("===== TEMPERATURE CONVERTER =====")
+print("1. Celsius to Fahrenheit")
+print("2. Fahrenheit to Celsius")
+
+choice = input("Enter your choice: ")
+
+if choice == "1":
+    celsius_to_fahrenheit()
+
+elif choice == "2":
+    fahrenheit_to_celsius()
+
+else:
+    print("Invalid choice!")rature: {celsius}°C")
+
+
+print("===== TEMPERATURE CONVERTER =====")
+print("1. Celsius to Fahrenheit")
+print("2. Fahrenheit to Celsius")
+
+choice = input("Enter your choice: ")
+
+if choice == "1":
+    celsius_to_fahrenheit()
+
+elif choice == "2":
+    fahrenheit_to_celsius()
+
+else:
+    print("Invalid choice!")
